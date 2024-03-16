fa765cb5d0e68559f02ff16e7eefc69483c43e40
Ahmad Mattar <107460097+AhmadMTR88@users.noreply.github.com>
2024-03-16 21:20:56 +0300

    Done by mattar

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Bleems_Task.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Bleems_Task.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
---
+++ Models/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bleems_Task.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOndate { get; set; }
+    }
+}
