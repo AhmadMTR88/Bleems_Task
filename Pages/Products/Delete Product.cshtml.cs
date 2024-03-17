c56425a31f3eea7b20a36dd98efb723206f05a1e
Ahmad Mattar <107460097+AhmadMTR88@users.noreply.github.com>
2024-03-17 15:25:58 +0300

    Done by mattar

---
+++ Pages/Products/Delete Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bleems_Task.Pages.Products
+{
+    public class Delete_ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
