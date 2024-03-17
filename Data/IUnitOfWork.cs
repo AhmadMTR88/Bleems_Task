0ce83a9f8363c4b4d4a6ebc3cd464ffa725e4c37
Ahmad Mattar <107460097+AhmadMTR88@users.noreply.github.com>
2024-03-17 14:01:08 +0300

    asdas

---
+++ Data/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Bleems_Task.Models;
+
+namespace Bleems_Task.Data
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Product> ProductRepository { get; }
+        IRepository<ProductCategory> CategoryRepository { get; }
+        void Save();
+    }
+}
---
+++ Data/Repository.cs
@@ -0,0 +1,40 @@
+﻿namespace Bleems_Task.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
